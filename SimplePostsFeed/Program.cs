d8eee85b09e08883993790edfa0469ec16f0670d
Semyon Tagilcev <s-tagiltsev2016@yandex.ru>
2022-07-22 23:45:12 +0300

    wip

--- SimplePostsFeed/Program.cs
+++ SimplePostsFeed/Program.cs
@@ -18,6 +18,10 @@ namespace SimplePostsFeed
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls("http://*:5000");
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
